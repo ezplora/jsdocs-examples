41f710b1c948ba0ff9c2397b9ba1bc4ee2c2df79
Kofi <expl00ra@protonmail.com>
2019-03-18 15:00:25 +0000

    first commit

---
+++ example1.js
@@ -0,0 +1,28 @@
+/**
+ * I explore the use of JsDocs for documenting Javascript code
+ */
+
+/** A description of a function */
+ function foo(){
+
+ }
+
+ /** Represents a contract
+  * @param {string} policyId - ID of policy
+  * @param {string} consumerId - ID of consumer
+  * @param {string} businessId - ID of business
+  * @param {decimal} premium - Value of monthly premium 
+  */
+ function policy(policyId, consumerId, businessId, premium){
+
+ }
+
+/** Adds two numbers use {@link policy#policyId}
+ * 
+ * @param {integer} a - number 1
+ * @param {integer} b - number 2 
+ * 
+ */
+function add(a, b){
+    
+}
